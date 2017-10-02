65c8aa932bad7b6fcc6be9f50d7d602f3dfaf172
Eric Johnson <0216johnsoneric@gmail.com>
2017-10-02 22:28:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // const body = document.getElementById('body')
-  // if {
-  //   body.addEventListener('keydown', function(e) {
-  //   console.log(e.which)
-  //   return alert('Congradulations!')
-  // }
+  let index = 0;
+  const body = document.body
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+   	console.log(key);
+	  if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+          alert("Hurray!");  
+          index = 0;
+        }
+    } else {
+      index = 0;
+    }
+  }
 }
-  // Write your JavaScript code inside the init() function
+  
