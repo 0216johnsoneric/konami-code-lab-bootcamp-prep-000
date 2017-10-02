06595b3e59b2afb04fea474a058385eceb7e440a
Eric Johnson <0216johnsoneric@gmail.com>
2017-10-02 21:35:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  const input = document.getElementById('body')
+  body.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
+
   // Write your JavaScript code inside the init() function
 
 }
